cc4057d73330e59004deef446986eb7a0222e52e
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-21 10:52:41 +0300

    Update youtube.py

--- Youtube/youtube.py
+++ Youtube/youtube.py
@@ -49,7 +49,7 @@ async def process_youtube_link(client, message):
                 await downloading_msg.delete()
                 await uploading_msg.delete()
 
-                await message.reply_text("\n\nOWNER : @LISA_FAN_LK 💕\n\nSUCCESSFULLY UPLOADED!")
+                await message.reply_text("\n\nOWNER : @lllcz 🍓\n\nSUCCESSFULLY UPLOADED!")
             else:
                 logging.error("No video streams found.")
                 await message.reply_text("Error: No downloadable video found.")
