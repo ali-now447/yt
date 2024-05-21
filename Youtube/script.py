7a2ad29863cc67e2805204e021ff6f4e885b5c53
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-21 10:51:49 +0300

    Update script.py

--- Youtube/script.py
+++ Youtube/script.py
@@ -4,12 +4,12 @@ class Translation(object):
 
     ABOUT_TXT = """
 ╭───────────⍟
-├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/YouTubeUploaderOneBot)
+├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/MYtubeMbot)
 ├📢 **Framework** : [Pyrogram 2.0.106](https://docs.pyrogram.org/)
 ├💮 **Language** : [Python 3.12.3](https://www.python.org)
-├👥 **Support Group** : [NT BOTS SUPPORT](https://t.me/NT_BOTS_SUPPORT)
-├🥏 **Channel** : [NT BOT CHANNEL](https://t.me/NT_BOT_CHANNEL)
-├⛲ **Source** : [Click Here](https://github.com/LISA-KOREA/YouTube-Video-Download-Bot)
-├🎓 **Developer** : [LISA 👑](https://t.me/LISA_FAN_LK)
+├👥 **Support Group** : [NT BOTS SUPPORT](https://t.me/lllcz)
+├🥏 **Channel** : [NT BOT CHANNEL](https://t.me/mmmsc)
+├⛲ **Source** : [Click Here](https://github.com/LISA-KOREA/lllcz)
+├🎓 **Developer** : [LISA 👑](https://t.me/lllby )
 ╰───────────────⍟
 """
